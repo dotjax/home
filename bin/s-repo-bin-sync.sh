3897b10785ca054b6fdb21a1238586cbb1d231b7
dotjax <jacob.knepper@outlook.com>
2025-12-30 21:50:24 -0600

    Changes.

--- bin/s-repo-bin-sync.sh
+++ bin/s-repo-bin-sync.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "This script is mean to be run from the repository root. It will not work otherwise and may overwrite files unintentionally."
+
 cp -auv bin/* ~/bin/
 
 echo "Sync complete."
